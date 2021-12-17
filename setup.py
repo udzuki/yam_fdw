f277b97fbde14716b7d2391b123bc3edf9fe9151
hiroki-sawano <hiroki.sawano.2512@gmail.com>
2021-12-17 17:50:57 +0900

    Do not use pymongo 4 that requires python 3.6+

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
           description='Yet Another Postgres FDW for MongoDB',
           url='http://github.com/asya999/yam_fdw',
           version='0.7.1',
-          install_requires=['pymongo>=2.8.1',
+          install_requires=['pymongo>=2.8.1,<4',
                             'python-dateutil'],
           packages=['yam_fdw'])
 
